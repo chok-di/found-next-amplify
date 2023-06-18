68b6a95186c02e01b98152f8334e5e18867499d5
Di Wu <dwu233@gmail.com>
2023-06-18 18:20:27 -0400

    migrate react app from old repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,46 @@
-
-import Image from 'next/image'
-
+import Link from 'next/link';
+// import Date from '../components/date';
+import Head from 'next/head';
+import Layout, { siteTitle } from './layout';
+import utilStyles from '../styles/utils.module.css';
+import homeStyles from '../styles/home.module.css';
+// import { getSortedPostsData } from '../lib/posts';
+import { GetStaticProps } from 'next';
 
 
 export default function Home() {
   return (
-    <>
-      <h1>Static page</h1>
-      {/* <p>This page is static. It was built on {formattedDate}.</p>
-      <p>
-        <a href="/ssr">View a server-side rendered page.</a>
-      </p> */}
-    </>
+    <Layout home>
+      <div className = {`${homeStyles.top_page}`}>
+        <span><h1>Let's Thrive Together.</h1></span>
+        <div><button>Book Now</button></div>
+      </div>
+
+      <div className={`${homeStyles.mission} row`}>
+        <div className={`${homeStyles.card} col-md-4`}>
+          <h3>UNPLUG</h3>
+          <div>Unplug and enjoy time away from digital distractions, and focus more on the present.</div>
+        </div>
+        <div className={`${homeStyles.card} col-md-4`}>
+          <h3>CONNECT</h3>
+          <div>Reconnext with ourselves, the ocean, nature and other like-minded individuals.</div>
+        </div>
+        <div className={`${homeStyles.card} col-md-4`}>
+          <h3>RECHARGE</h3>
+          <div>Recharge through daily yoga, water sports and activities,coaching sessions,seasonal menus, and comfortable accommodation.</div>
+        </div>
+      </div>
+
+      <div className="Gallery">
+        <h3>Photo Gallery</h3>
+        {/* <Gallery/> */}
+      </div>
+
+      <div className="Calendar">
+        <h3>Calendar</h3>
+        {/* <Scheduler/> */}
+      </div>
+    </Layout>
   );
 }
 
