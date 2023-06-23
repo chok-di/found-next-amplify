6ff9d1fcb7772cb174cf34e6467478d974e5377c
Di Wu <dwu233@gmail.com>
2023-06-22 22:42:40 -0400

    npm i dotenv

--- amplify/backend/function/foundtrips/src/index.js
+++ amplify/backend/function/foundtrips/src/index.js
@@ -1,17 +1,33 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-};
+import { Client } from "pg";
+
+ exports.handler = async (event) => {
+     const client = new Client({
+         host: process.env.RDS_HOSTNAME,
+         port: process.env.RDS_PORT,
+         user: process.env.RDS_USERNAME,
+         password: process.env.RDS_PASSWORD,
+         database: process.env.RDS_DB_NAME,
+     });
+ 
+     await client.connect();
+ 
+     let res;
+     try {
+         res = await client.query('SELECT * FROM YourTable');
+         console.log(res.rows);
+     } catch (err) {
+         console.error(err);
+     } finally {
+         await client.end();
+     }
+ 
+     const response = {
+         statusCode: 200,
+         body: JSON.stringify(res.rows),
+     };
+ 
+     return response;
+ };
