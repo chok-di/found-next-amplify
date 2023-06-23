2d9375fceb40e92925b0be80c1e56bd242496d13
Di Wu <dwu233@gmail.com>
2023-06-22 23:05:10 -0400

    complete lambda function for get trip info

--- amplify/backend/function/foundtrips/src/index.js
+++ amplify/backend/function/foundtrips/src/index.js
@@ -3,31 +3,50 @@
  */
 import { Client } from "pg";
 
- exports.handler = async (event) => {
-     const client = new Client({
-         host: process.env.RDS_HOSTNAME,
-         port: process.env.RDS_PORT,
-         user: process.env.RDS_USERNAME,
-         password: process.env.RDS_PASSWORD,
-         database: process.env.RDS_DB_NAME,
-     });
- 
-     await client.connect();
- 
-     let res;
-     try {
-         res = await client.query('SELECT * FROM YourTable');
-         console.log(res.rows);
-     } catch (err) {
-         console.error(err);
-     } finally {
-         await client.end();
-     }
- 
-     const response = {
-         statusCode: 200,
-         body: JSON.stringify(res.rows),
-     };
- 
-     return response;
- };
+exports.handler = async (tripId) => {
+    const client = new Client({
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+    });
+
+    await client.connect();
+
+    let res;
+
+
+
+    try {
+        const res = await client.query(
+            `SELECT * FROM trips
+             WHERE id = $1`, [tripId]
+        );
+        console.log(res.rows[0]);
+    } catch (err) {
+        console.log(err.messages);
+
+    } finally {
+        await client.end();
+    }
+
+    //  try {
+    //      res = await 
+
+
+    //      client.query('SELECT * FROM YourTable');
+    //      console.log(res.rows);
+    //  } catch (err) {
+    //      console.error(err);
+    //  } finally {
+    //      await client.end();
+    //  }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(res.rows),
+    };
+
+    return response;
+};
