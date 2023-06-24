05d73cf1a0fcff461b9dec1eb8a430ff3a2a5da9
Di Wu <dwu233@gmail.com>
2023-06-24 01:13:18 -0400

    missed a }

--- amplify/backend/function/foundtrips/src/index.js
+++ amplify/backend/function/foundtrips/src/index.js
@@ -2,28 +2,28 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
 
  */
- 
- const { connectToDatabase } = require('/opt/nodejs/common');
 
- exports.handler = async () => {
+const { connectToDatabase } = require('/opt/nodejs/common');
 
-     const client = await connectToDatabase();
-     await client.connect();
-  
-     try {
-         const res = await client.query(
-             `SELECT * FROM trips`
-         );
-         const response = {
-         statusCode: 200,
-         body:res.rows,
-     };
-      return response;
-     
-     } catch (err) {
-         console.log(err.messages);
- 
-     } finally {
-         await client.end();
-     }
- 
+exports.handler = async () => {
+
+    const client = await connectToDatabase();
+    await client.connect();
+
+    try {
+        const res = await client.query(
+            `SELECT * FROM trips`
+        );
+        const response = {
+            statusCode: 200,
+            body: res.rows,
+        };
+        return response;
+
+    } catch (err) {
+        console.log(err.messages);
+
+    } finally {
+        await client.end();
+    }
+}
