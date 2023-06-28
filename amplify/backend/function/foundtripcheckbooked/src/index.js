5a44a24a291077ff75f496896a060c6c5cf3b77c
Di Wu <dwu233@gmail.com>
2023-06-28 13:34:41 -0400

    configure aws lambda function

--- amplify/backend/function/foundtripcheckbooked/src/index.js
+++ amplify/backend/function/foundtripcheckbooked/src/index.js
@@ -11,20 +11,21 @@
      await client.connect();
  
      try {
-        const tripId = params.tripId
-         const res = await client.query(
-            `SELECT * FROM trips
-             WHERE id = $1`,[tripId]
+        const tripId = params.tripId;
+        const email = params.email;
+        const res = await client.query(
+            `SELECT EXISTS
+             (SELECT 1 FROM bookings
+             WHERE trip_id = $1 and email = $2)`,
+             [tripId,email]
           );
          const response = {
              statusCode: 200,
-             body: res.rows,
+             body: res.rows[0].exists,
          };
          return response;
- 
      } catch (err) {
-         console.log(err.messages);
- 
+         console.log(err);
      } finally {
          await client.end();
      }
--- src/pages/trip/[id].jsx
+++ src/pages/trip/[id].jsx
@@ -52,56 +52,28 @@ export async function getServerSideProps(context) {
   });
 
   let trip;
-  let booked;
+  let isBooked;
   try {// await the Lambda invocat
-    [trip, booked] = await Promise.all([lambdaPromise1,lambdaPromise2]);
+    [trip, isBooked] = await Promise.all([lambdaPromise1,lambdaPromise2]);
   } catch (err) {
     console.error("Failed to fetch trips", err);
     trip = null;
   }
-  return { props: { trip,booked} }
+  return { props: { trip, isBooked } }
 }
 
 
 
-const EventDetailPage = ({trip}) => {
-  console.log({trip})
+const EventDetailPage = ({trip,isBooked}) => {
+  console.log({trip});
+  console.log({isBooked});
   trip = JSON.parse(trip.Payload).body[0];
   const description = trip.description.split("&").map(line => <p>{line}</p>);
 
   const [confirm,setConfirm] = useState(false);
-  const [booked,setBooked] = useState(false);
+  const [booked,setBooked] = useState(isBooked);
   // const [status,setStatus] = useState(false);
   
-
-  // const {id} = useParams();
-  
-  // useEffect(()=>{
-  //   const loadTrip = async() => {
-  //     try{
-  //       const tripInfo = await axios.get(`/trips/${id}`);
-  //       // console.log(tripInfo)
-  //       setTrip(tripInfo.data);
-  //       const booked = await axios.get(`/trips/book/${id}/${props.user.id}`);
-  //       if (booked.data == "TRUE")  setBooked(true);
-  //       // return booked.rows
-  //     } catch(err){
-  //       console.log(err.message);
-  //     }
-  //   };
-
-    // const isBooked = async() => {
-    //   try{
-        
-    //     console.log(booked);
-    //   } catch(err){
-    //     console.log(err.message);
-    //   }
-    // }
-  //   loadTrip();
-
-  // },[]);
-   
 
 
   // const bookTrip = async(e) => {
