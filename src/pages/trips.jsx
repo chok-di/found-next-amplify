6501a4abcbddd16388e8f5e65041fe28d5707ff8
Di Wu <dwu233@gmail.com>
2023-07-27 00:17:01 -0400

    remove redundant aws import on trips page

--- src/pages/trips.jsx
+++ src/pages/trips.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import AWS from 'aws-sdk';
-import Cookies from 'js-cookie';
 import TripCard from "../app/components/TripCard";
 import {getToken} from "../hooks/checkUserGetEmail.js";
 import {getAllTrips, getAllBookings} from "../hooks/getTripInfo.js";
@@ -8,54 +6,11 @@ import {getAllTrips, getAllBookings} from "../hooks/getTripInfo.js";
 // import Scheduler from "../components/Scheduler";
 
 
-AWS.config.region = "us-east-2";
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: "us-east-2:7dc220ca-2c98-428d-86c2-83fa56c53ebd"
-});
 
 export async function getServerSideProps(context) {
   const token = context.req.cookies.userToken;
   const user = token? await getToken(token) : null;
   const email = user? user.decoded.email : null;
-
-
-  // const lambda = new AWS.Lambda();
-  // const params1 = {
-  //   FunctionName: 'foundtrips-dev',
-  //   InvocationType: 'RequestResponse',
-  //   LogType: 'Tail',
-  //   Payload: JSON.stringify({ key1: 'value1', key2: 'value2' })
-  // };
-
-  // const params2 = {
-  //   FunctionName: 'foundallbookings-dev',
-  //   InvocationType: 'RequestResponse',
-  //   LogType: 'Tail',
-  //   Payload: JSON.stringify({ key1: 'value1', key2: 'value2' })
-  // };
-
-  // const lambdaPromise1 = new Promise((resolve, reject) => {
-  //   lambda.invoke(params1, function (err, data) {
-  //     if (err) {
-  //       console.error("Error invoking Lambda function", err);
-  //       reject(err);
-  //     } else {
-  //       resolve(data);
-  //     }
-  //   });
-  // });
-
-  // const lambdaPromise2 = new Promise((resolve, reject) => {
-  //   lambda.invoke(params2, function (err, data) {
-  //     if (err) {
-  //       console.error("Error invoking Lambda function", err);
-  //       reject(err);
-  //     } else {
-  //       resolve(data);
-  //     }
-  //   });
-  // });
-
 
   let trips, bookings;
   try {
@@ -70,9 +25,8 @@ export async function getServerSideProps(context) {
 
 
 
-
-
 const BookEventPage = ({trips,bookings, email}) => {
+  
   const bookingsInformation = JSON.parse(bookings.Payload).body;
   const tripInformation = JSON.parse(trips.Payload).body
     .map((trip) => {
@@ -82,8 +36,7 @@ const BookEventPage = ({trips,bookings, email}) => {
 
       const is_full = bookedUser.length == trip.total_spots
       const is_booked = email in bookedUser;
-      console.log("booked?");
-      console.log(is_booked);
+
       return (
         <>
           <TripCard
@@ -98,7 +51,6 @@ const BookEventPage = ({trips,bookings, email}) => {
         </>
       );
     });
-
 
   return (
     <>
