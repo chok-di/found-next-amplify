f677b4ad027befc04d055f85ba396a2d960809d7
Di Wu <dwu233@gmail.com>
2023-07-30 05:24:16 -0400

    refactored getInitialProps

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import cookies from 'js-cookie';
 import {useRouter} from 'next/router';
 import {Amplify,Auth} from 'aws-amplify';
 import awsmobile from '../aws-exports.js';
@@ -6,45 +7,50 @@ import Layout from '../app/components/Layout';
 import Nav from '../app/components/Nav';
 import Footer from '../app/components/Footer';
 
-import {getToken} from "../hooks/checkUserGetEmail.js";
+import {getUser} from "../hooks/checkUserGetEmail.js";
 
 Amplify.configure({...awsmobile, ssr:true});
 
-export default function MyApp({Component,pageProps,user}){
+export default function MyApp({Component,pageProps,email}){
 
   const router = useRouter(); 
   // const [user, setUser] = useState(null);
-  const logOut = () => {
-    Auth.signOut()
-    .then(setUser(null))
-    .catch(err => console.log(err));
-  }
+  // const logOut = () => {
+  //   Auth.signOut()
+  //   .then(setUser(null))
+  //   .catch(err => console.log(err));
+  // }
 
-  useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then(authData => {
-       setUser(authData);
-      })
-      .catch(err => {
-        console.log(err);
-        // handle situation when user is not authenticated or any other auth error
-      });
-  }, [router.asPath]);
+  // useEffect(() => {
+  //   Auth.currentAuthenticatedUser()
+  //     .then(authData => {
+  //      setUser(authData);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       // handle situation when user is not authenticated or any other auth error
+  //     });
+  // }, [router.asPath]);
 
   return(
     <Layout>
-      <Nav user={user} logOut={logOut}/>
-      <Component {...pageProps} user={user} setUser={setUser}/>
+      <Nav user={email} />
+      <Component {...pageProps} user={email} />
       <Footer/>
     </Layout>
   )
 }
 
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
+MyApp.getInitialProps = async ({Component,ctx}) => {
+
+  let pageProps = {};
+  if (Component.getInitialProps){
+    const componentProps = await Component.getInitialProps(ctx);
+    pageProps = {...componentProps};
+  }
+  const token = cookies.get("userToken")
+  let user = await getUser(token);
   let email = null;
-  const token = appContext.req.cookies.userToken;
-  if (token)  user = await getToken(token);
-  if (user) email = user.decoded.email
-  return {...appProps, email}
+  if (user) email = user.email;
+  return {...pageProps, email}
 }
