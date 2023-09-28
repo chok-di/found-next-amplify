c2ca5e55129fc75990ad622f58ba7e5c611ceb46
Di Wu <dwu233@gmail.com>
2023-09-28 08:12:28 -0400

    alignment of nav and cover page is fixed

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -3,9 +3,12 @@ import Head from 'next/head';
 // import Image from 'next/image';
 // import styles from './layout.module.css';
 // import utilStyles from '../styles/utils.module.css';
-// import Link from 'next/link';
+import Link from 'next/link';
 const name = 'Woody';
 const siteTitle = 'Next.js Sample Website';
+
+import Nav from "./Nav.jsx";
+import Footer from "./Footer.jsx";
 
 //font awesome
 
@@ -14,50 +17,78 @@ import { Marcellus, Jost, Inter } from 'next/font/google';
 
 
 const marcellus = Marcellus({
-  subsets:['latin'],
-  weight:['400'],
-  variable:'--font-marcellus',
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-marcellus',
 })
 
 const jost = Jost({
-  subsets:['latin'],
-  weight:['600','400'],
-  variable:'--font-jost',
+  subsets: ['latin'],
+  weight: ['600', '400'],
+  variable: '--font-jost',
 })
 
 const inter = Inter({
-  subsets:['latin'],
-  weight:['400'],
-  variable:'--font-inter'
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-inter'
 })
 
 
 export default function RootLayout({
-  children,
+  children, home
 }: {
   children: React.ReactNode
-  // home?: boolean
+  home?: boolean
 }) {
   return (
     <main className={`${marcellus.variable} ${jost.variable} ${inter.variable}`}>
-    <html lang="en">
-      <Head>
-        <link rel='icon' href='/favicon.ico'/>
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(siteTitle,)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <body>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <Head>
+          <link rel='icon' href='/favicon.ico' />
+          <meta
+            name="description"
+            content="Learn how to build a personal website using Next.js"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(siteTitle,)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <body className="relative">
+          {home ? (
+            
+              <div className="relative z-10 overflow-y-hidden">
+                <div className="absolute top-0 left-0 w-full h-[700px] bg-cover bg-[url('../img/background.jpg')]">
+                  <Nav />
+                  <span><h1 className="ml-24 pt-48 text-7xl font-serif text-white ">Let's Thrive Together.</h1></span>
+                  <div>
+                    <Link href="/trips">
+                      <button
+                        className="ml-24 mt-24 mb-24 font-mono font-semibold text-ocean bg-white w-40 h-10 shadow-[2px_4px_4px_4px_rgba(0,0,0,0.4)]">
+                        Book Now &#8594;
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+                {children}
+                <Footer />
+              </div>
+            
+
+
+          ) : (
+            <>
+              <Nav />
+              {children}
+              <Footer />
+            </>
+
+          )}
+        </body>
+      </html>
     </main>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,52 +2,42 @@
 import 'dotenv/config';
 import Link from 'next/link';
 
-
+import Layout from "../app/components/Layout";
 import Gallery from "../app/components/Gallery";
 import Scheduler from "../app/components/Scheduler";
+import Nav from "../app/components/Nav";
 
 
 
 export default function Home() {
   return (
-
-    <div className="m-0 p-0">
-      <div className=" m-0 p-0 bg-[url('../img/background.jpg')]" >
-        <span><h1 className="ml-24 pt-48 text-7xl font-serif text-white ">Let's Thrive Together.</h1></span>
-        <div>
-          <Link href="/trips">
-          <button 
-          className="ml-24 mt-24 mb-24 font-mono font-semibold text-ocean bg-white w-40 h-10 shadow-[2px_4px_4px_4px_rgba(0,0,0,0.4)]">
-            Book Now &#8594; 
-          </button>
-          </Link>
+    <Layout home>
+      <div className="relative mt-[700px]">
+        <div className="flex flex-row bg-grey h-72">
+          <div className="basis-1/3 text-center">
+            <h3 className="font-serif text-2xl mt-16">UNPLUG</h3>
+            <div className="font-jost mx-24 mt-4">Unplug and enjoy time away from digital distractions, and focus more on the present.</div>
+          </div>
+          <div className="basis-1/3 text-center">
+            <h3 className="font-serif text-2xl mt-16">CONNECT</h3>
+            <div className="font-jost mx-24 mt-4">Reconnext with ourselves, the ocean, nature and other like-minded individuals.</div>
+          </div>
+          <div className="basis-1/3 text-center">
+            <h3 className="font-serif text-2xl mt-16">RECHARGE</h3>
+            <div className="font-jost mx-24 mt-4">Recharge through daily yoga, water sports and activities,coaching sessions,seasonal menus, and comfortable accommodation.</div>
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-row bg-grey h-72">
-        <div className="basis-1/3 text-center">
-          <h3 className="font-serif text-2xl mt-16">UNPLUG</h3>
-          <div className="font-jost mx-24 mt-4">Unplug and enjoy time away from digital distractions, and focus more on the present.</div>
+        <div className="bg-#ffffff">
+          <h4 className="font-serif text-xl py-16 text-center">Photo Gallery</h4>
+          <Gallery />
         </div>
-        <div className="basis-1/3 text-center">
-          <h3 className="font-serif text-2xl mt-16">CONNECT</h3>
-          <div className="font-jost mx-24 mt-4">Reconnext with ourselves, the ocean, nature and other like-minded individuals.</div>
-        </div>
-        <div className="basis-1/3 text-center">
-          <h3 className="font-serif text-2xl mt-16">RECHARGE</h3>
-          <div className="font-jost mx-24 mt-4">Recharge through daily yoga, water sports and activities,coaching sessions,seasonal menus, and comfortable accommodation.</div>
-        </div>
-      </div>
 
-      <div className="bg-#ffffff">
-        <h4 className="font-serif text-xl py-16 text-center">Photo Gallery</h4>
-        <Gallery />
-      </div>
-
-      <div className="bg-#ffffff">
-        <h4 className="font-serif text-xl py-16 text-center">Calendar</h4>
-        <Scheduler />
-      </div>
-    </div>
+        <div className="bg-#ffffff">
+          <h4 className="font-serif text-xl py-16 text-center">Calendar</h4>
+          <Scheduler />
+        </div>
+        </div>
+    </Layout>
   );
 }
